4326976ca764b2bf2f6d01a40b411fce459c1dbe
Redolff <redolfofederico@gmail.com>
2025-09-20 20:20:19 -0300

    add new script, server connection, socket.io

--- clase-6/server/index.js
+++ clase-6/server/index.js
@@ -1,15 +1,32 @@
 import express from 'express'
 import logger from 'morgan'
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
 
 const PORT = process.env.PORT || 3000
 
 const app = express()
+const server = createServer(app)
+const io = new Server(server)
+
+io.on('connection', (socket) => {
+    console.log('a user has connected!')
+    
+    socket.on('disconnect', () => {
+        console.log('an user has disconnect!')
+    })
+
+    socket.on('chat message', (message) => {
+        console.log('message: '+ message)
+    })
+})
+
 app.use(logger('dev')) /// ---> DEPENDENCIA PARA ACCEDER 
 
 app.get('/', (req, res) => {
     res.sendFile(process.cwd() + '/client/index.html')
 })
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`)
 })
