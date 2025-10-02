2e8c02d45f8f5ddf30e3df3b8c4f6a37ad1d1c1c
Redolff <redolfofederico@gmail.com>
2025-10-02 02:14:08 -0300

    Add new clase-7, new middlewares, cookies, JWT, authetnicacion, rutas, HTML, CSS, NodeJS

--- clase-7/index.js
+++ clase-7/index.js
@@ -1,25 +1,42 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
+import cookieParser from 'cookie-parser'
 import { UserRepository } from './user-repository.js'
+import 'dotenv/config'
+import { middlewareToken } from './middlewares/middlewareToken.js'
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT
 const app = express()
-app.set('view engine', 'ejs')
-app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.render('example', { username: 'Federico Redolfo' })
+app.set('view engine', 'ejs')
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(middlewareToken)
+
+app.get('/', (req, res) => {    
+    const { user } = req.session
+    res.render('index', user)
 })
 
 app.post('/login', async (req, res) => {
     const { username, password } = req.body
-
     try {
         const user = await UserRepository.login({ username, password })
-        const token = jwt.sign({ id: user._id, username: user.username }, process.env.SECRET_JWT_KEY, {
-            expiresIn: '1h'
-        })
-        res.send({ user })
+        const token = jwt.sign(
+            { id: user._id, username: user.username },
+            process.env.SECRET_JWT_KEY, 
+            { expiresIn: '1h'}
+        )
+
+        res
+            .cookie('access_token', token, {
+                httpOnly: true, // la cookie solo puede accederse desde el servidor
+                secure: process.env.NODE_ENV === 'production', // la cookie solo puede accederse en https
+                sameSite: 'strict', // la cookie solo puede accederse en el mismo dominio
+                maxAge: 1000 * 60 * 60 // la cookie solo tiene validez de tiempo de 1 hs
+            })
+            .send({ user, token })
     }catch(error){
         res.status(401).send(error.message)
     }
@@ -27,7 +44,6 @@ app.post('/login', async (req, res) => {
 
 app.post('/register', async (req, res) => {
     const { username, password } = req.body
-    console.log(req.body)
 
     try {
         const id = await UserRepository.create({ username, password })
@@ -37,10 +53,15 @@ app.post('/register', async (req, res) => {
     }
 })
 
-app.post('/logout', (req, res) => {})
+app.post('/logout', (req, res) => {
+    res.clearCookie('access_token')
+    .json({ message: 'Logout successful' })
+})
 
 app.get('/protected', (req, res) => {
-    res.render('protected', { username: 'Federico' })
+    const { user } = req.session
+    if(!user) return res.status(403).send('Access not authorized')
+    res.render('protected', user)
 })
 
 app.listen(PORT, () => {
--- clase-7/user-repository.js
+++ clase-7/user-repository.js
@@ -13,15 +13,16 @@ const User = Schema('User', {
 export class UserRepository {
     static async create ({ username, password }) {
         // 1. Validaciones (con ZOD puedo hacerlas)
-        Validation.username(username)
-        Validation.password(password)
+        Validation.username({ username })
+        Validation.password({ password })
 
         // 2. Asegurarse que el username no existe
         const user = User.findOne({ username })
         if(user) throw new Error('Username already exists')
 
         const id = crypto.randomUUID() // --> Las genera las DB
-        const hashedPassword = await bcrypt.hash(password, process.env.SALT_ROUNDS)
+        const SALT_ROUNDS = Number(process.env.SALT_ROUNDS)
+        const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS)
 
         User.create({
             _id: id,
@@ -33,8 +34,8 @@ export class UserRepository {
     }
 
     static async login ({ username, password }) {
-        Validation.username(username )
-        Validation.password(password)
+        Validation.username({ username })
+        Validation.password({ password })
 
         const user = User.findOne({ username })
         if(!user) throw new Error('username does not exists')
@@ -50,12 +51,12 @@ export class UserRepository {
 
 class Validation {
     static username ({ username }) {
-        if(typeof username !== string) throw new Error('Username must be a string')
+        if(typeof username !== 'string') throw new Error('Username must be a string')
         if(username.length < 3) throw new Error('Username must be at least 3 characters long')
     }
 
     static password ({ password }) {
-        if(typeof password !== string) throw new Error('Password must be a string')
+        if(typeof password !== 'string') throw new Error('Password must be a string')
         if(password.length < 6) throw new Error('Password must be at least 6 characters long')
 
     }
